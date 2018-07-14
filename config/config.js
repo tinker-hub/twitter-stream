ea71633cab217815f5afb7c7ee923b1dedfb515f
jmaicaaan <jmsantosss29@yahoo.com>
2018-07-14 11:51:53 +0800

    Only supported one stream track as user is a standard user

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  twitter: {
+    consumer_key: process.env.CONSUMER_KEY || '2IrcQ0cXKk8jEJcL37blzaYY3',
+    consumer_secret: process.env.CONSUMER_SECRET || 'nHwKhCjHov5CRNdoNChjcHKTPG9tTFIK8pdtnGWBf8eMy0Mxi0',
+    access_token_key: process.env.ACCESS_TOKEN_KEY || '1017975558284632064-vvOZv1wutuJVc8nTQpNbMWgwssvR6B',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET || '1CMxlwsdvZ9lXOQ5ccu7SMqOv8s4ocjYibeXD6VA2VZcc'
+  },
+  streamTracks: [
+    // 'from:officialLRT1',
+    '@officialLRT1'
+  ]
+}
--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const Twitter = require('twitter');
 
 const config = require('./config/config');
 
-const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY || config.consumer_key,
-  consumer_secret: process.env.CONSUMER_SECRET || config.consumer_secret,
-  access_token_key: process.env.ACCESS_TOKEN_KEY || config.access_token_key,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET || config.access_token_secret
-});
+const streamTracks = config.streamTracks;
+const client = new Twitter(config.twitter);
 
 const streamTo = (trackFilter = '', streamHandler = (err, tweet) => {}) => {
   client.stream('statuses/filter', { track: trackFilter }, (stream) => {
@@ -20,15 +16,11 @@ const streamTo = (trackFilter = '', streamHandler = (err, tweet) => {}) => {
   });
 };
 
-
-streamTo('@officialLRT1', (err, tweet) => {
-  if (err) return console.log('error on stream', err);
-  if (!tweet.retweeted) {
-    console.log('Tweet to @officialLRT1 -', tweet.text);
-  }
-});
-
-streamTo('from:officialLRT1', (err, tweet) => {
-  if (err) return console.log('error on stream', err);
-  console.log('Tweet by @officialLRT1 -', tweet.text);
+streamTracks.forEach((streamTrack) => {
+  streamTo(streamTrack, (err, tweet) => {
+    if (err) return console.log('error on stream', err);
+    if (!tweet.retweeted) {
+      console.log(`Tweet to ${tweet.user.name} -`, tweet.text);
+    }
+  });
 });
